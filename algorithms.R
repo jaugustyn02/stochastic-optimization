4d74f31b3dd14956ec4b5fdb5cf664924124ac17
Jan Augustyn <jan.augusty02@gmail.com>
2023-01-27 12:24:09 +0100

    final final

--- algorithms.R
+++ algorithms.R
@@ -1,16 +1,12 @@
 # Pure Random Search 
 PRS <- function(dims, num_points, fn) {
   num_dims = nrow(dims)
-  min_point <- rep(0, num_dims)
   min_val <- Inf
   
   for(i in 1:num_points){
     point <- runif(num_dims, dims[1,1], dims[1,2])
     val <- fn(point)
-    if (val < min_val){
-      min_point <- point
-      min_val <- val
-    }
+    min_val <- min(min_val, val)
   }
   return(min_val)
 }
@@ -19,16 +15,12 @@ PRS <- function(dims, num_points, fn) {
 # Multi-Start
 MS <- function(dims, num_points, fn){
   num_dims = nrow(dims)
-  min_point <- rep(0, num_dims)
   min_val <- Inf 
   for(i in 1:num_points){
     point <- runif(num_dims, dims[1,1], dims[1,2])
     point <- optim(par = point, fn = fn, method = "L-BFGS-B", lower = dims[,1], upper = dims[,2])$par
     val <- fn(point)
-    if (val < min_val){
-      min_point <- point
-      min_val <- val
-    }
+    min_val <- min(min_val, val)
   }
   return(min_val)
 }
